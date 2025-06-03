169753a738d88a5e10ba21341b9ababb75df3ee8
Loïc <nephtyse19@hotmail.fr>
2025-06-03 11:59:14 +0200

    feat(demo-binding): add demo binding component with template, styles, and tests

---
+++ src/app/demo-binding/demo-binding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-binding',
+  imports: [],
+  templateUrl: './demo-binding.component.html',
+  styleUrl: './demo-binding.component.scss'
+})
+export class DemoBindingComponent {
+
+}
