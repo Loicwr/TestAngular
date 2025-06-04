6f55009c1fc8d00a9c37a76423394e4b12cd5e65
Loicwr <nephtyse19@hotmail.fr>
2025-06-04 12:10:05 +0200

    feat(app): add DirectivesStructurellesComponent to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "./header/header.component";
 import { DemoBindingComponent } from "./demo-binding/demo-binding.component";
+import { DirectivesStructurellesComponent } from "./directives-structurelles/directives-structurelles.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, DemoBindingComponent],
+  imports: [RouterOutlet, HeaderComponent, DemoBindingComponent, DirectivesStructurellesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
