45ca2a09eda97efe1ce7321d8a8b663a66d57a4e
Loicwr <nephtyse19@hotmail.fr>
2025-06-04 11:58:13 +0200

    feat(directives-structurelles): add initial component, template, styles, and tests

---
+++ src/app/directives-structurelles/directives-structurelles.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DirectivesStructurellesComponent } from './directives-structurelles.component';
+
+describe('DirectivesStructurellesComponent', () => {
+  let component: DirectivesStructurellesComponent;
+  let fixture: ComponentFixture<DirectivesStructurellesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DirectivesStructurellesComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DirectivesStructurellesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/directives-structurelles/directives-structurelles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directives-structurelles',
+  imports: [],
+  templateUrl: './directives-structurelles.component.html',
+  styleUrl: './directives-structurelles.component.scss'
+})
+export class DirectivesStructurellesComponent {
+
+}
