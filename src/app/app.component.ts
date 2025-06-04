88359236b824bc9076ae7b4828a7586f6af06eb7
Loicwr <nephtyse19@hotmail.fr>
2025-06-04 15:37:56 +0200

    feat(pipes): add PipesComponent with initial template and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from "./header/header.component";
 import { DemoBindingComponent } from "./demo-binding/demo-binding.component";
 import { DirectivesStructurellesComponent } from "./directives-structurelles/directives-structurelles.component";
 import { DirectivesDAttributComponent } from "./directives-d-attribut/directives-d-attribut.component";
+import { PipesComponent } from "./pipes/pipes.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, DemoBindingComponent, DirectivesStructurellesComponent, DirectivesDAttributComponent],
+  imports: [RouterOutlet, HeaderComponent, DemoBindingComponent, DirectivesStructurellesComponent, DirectivesDAttributComponent, PipesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
