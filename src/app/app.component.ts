a8f0f5e696b26dace0663341212f2772f66057e1
Loicwr <nephtyse19@hotmail.fr>
2025-06-04 12:16:46 +0200

    fix(directives-structurelles): import CommonModule in DirectivesStructurellesComponent refactor(app): remove unused CommonModule import from AppComponent spec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "./header/header.component";
 import { DemoBindingComponent } from "./demo-binding/demo-binding.component";
 import { DirectivesStructurellesComponent } from "./directives-structurelles/directives-structurelles.component";
--- src/app/directives-structurelles/directives-structurelles.component.ts
+++ src/app/directives-structurelles/directives-structurelles.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-directives-structurelles',
-  imports: [],
+  imports: [CommonModule, ],
   templateUrl: './directives-structurelles.component.html',
   styleUrl: './directives-structurelles.component.scss'
 })
