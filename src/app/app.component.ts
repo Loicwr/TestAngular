8b790763f21da8891f50ee09462ba4e3687cd2ab
Loicwr <nephtyse19@hotmail.fr>
2025-06-04 15:38:59 +0200

    feat(header): remove HeaderComponent and associated files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
 import { DemoBindingComponent } from "./demo-binding/demo-binding.component";
 import { DirectivesStructurellesComponent } from "./directives-structurelles/directives-structurelles.component";
 import { DirectivesDAttributComponent } from "./directives-d-attribut/directives-d-attribut.component";
@@ -8,7 +7,7 @@ import { PipesComponent } from "./pipes/pipes.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, DemoBindingComponent, DirectivesStructurellesComponent, DirectivesDAttributComponent, PipesComponent],
+  imports: [RouterOutlet, DemoBindingComponent, DirectivesStructurellesComponent, DirectivesDAttributComponent, PipesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
