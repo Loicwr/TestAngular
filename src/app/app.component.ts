305d9b0b27150f2732c999fba501f9c10dea5e1c
Loicwr <nephtyse19@hotmail.fr>
2025-06-04 22:25:41 +0200

    feat(module-angular): update module-angular component with new content and structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { DirectivesStructurellesComponent } from "./directives-structurelles/dir
 import { DirectivesDAttributComponent } from "./directives-d-attribut/directives-d-attribut.component";
 import { PipesComponent } from "./pipes/pipes.component";
 import { FormulairesAvecFormsModuleComponent } from "./formulaires-avec-forms-module/formulaires-avec-forms-module.component";
+import { ModuleAngularComponent } from "./module-angular/module-angular.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DemoBindingComponent, DirectivesStructurellesComponent, DirectivesDAttributComponent, PipesComponent, FormulairesAvecFormsModuleComponent],
+  imports: [RouterOutlet, DemoBindingComponent, DirectivesStructurellesComponent, DirectivesDAttributComponent, PipesComponent, FormulairesAvecFormsModuleComponent, ModuleAngularComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
